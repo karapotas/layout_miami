d4aeea94efd64ea5afa8e5cc37b950c97e98b2b3
Serhii Karapota <s.karapota@gmail.com>
2021-04-13 08:44:15 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function() {
-  const burger = document.querySelector('.nav__icon');
-
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('nav__icon--active');
-  });
-}());
